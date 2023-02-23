3814bc1e138e6a3421e5a8cb65cbd2187ee1be30
x0-3 <steph6363@gmail.com>
2023-02-23 15:53:09 +0100

    scroll to top animation

--- js/App.js
+++ js/App.js
@@ -14,5 +14,5 @@ function scrollFunction() {
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+  document.documentElement.scrollTo({top: 0, behavior: 'smooth'});
 }
