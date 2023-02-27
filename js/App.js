3360c3383d287e51b7e266f3be077d9fb44a77d3
x0-3 <steph6363@gmail.com>
2023-02-27 11:42:31 +0100

    mobile version

--- js/App.js
+++ js/App.js
@@ -49,3 +49,14 @@ function textColor(color){
     button[i].style.background = color;
   }
 }
+
+// menu phone
+function mobileNav() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
